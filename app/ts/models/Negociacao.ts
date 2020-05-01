6663d9430cc6df0c6ec9797c0c1cbd0ae1204d14
Phellipe Sander <phellipe.sander1@gmail.com>
2020-05-01 14:42:22 -0300

    configurando compilador

---
+++ app/ts/models/Negociacao.ts
@@ -0,0 +1,26 @@
+class Negociacao{
+
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+
+    get data(){
+        return this._data;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+}
